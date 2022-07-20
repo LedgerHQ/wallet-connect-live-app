fb52a96cfe9015a60b512bd43f6cbe4777e77c85
Hugo Morosini <hugo.morosini@gmail.com>
2022-07-20 17:24:00 +0200

    improving requestAccount button

--- src/components/WalletConnect/PendingRequest.tsx
+++ src/components/WalletConnect/PendingRequest.tsx
@@ -1,7 +1,5 @@
 import styled from 'styled-components'
-import { CSSTransition } from 'react-transition-group'
-import { Box, Button, Text } from '@ledgerhq/react-ui'
-import { IClientMeta } from '@walletconnect/types'
+import { Button, Flex, Text } from '@ledgerhq/react-ui'
 import {
 	CheckAloneRegular,
 	CloseRegular,
@@ -18,31 +16,34 @@ export type PendingRequestProps = {
 	onDecline: () => void
 }
 
-export function PendingRequest({
-	onAccept,
-	onDecline,
-}: PendingRequestProps) {
+export function PendingRequest({ onAccept, onDecline }: PendingRequestProps) {
 	return (
-			<PendingRequestContainer>
-				<Box mt="32px">
-					<Button
-						mr="24px"
-						variant="color"
-						Icon={CheckAloneRegular}
-						outline
-						onClick={onAccept}
-					>
-						<Text>Accept</Text>
-					</Button>
-					<Button
-						variant="error"
-						Icon={CloseRegular}
-						outline
-						onClick={onDecline}
-					>
-						<Text>Decline</Text>
-					</Button>
-				</Box>
-			</PendingRequestContainer>
+		<PendingRequestContainer>
+			<Flex
+				mt={8}
+				flexDirection="row"
+				flexWrap="wrap-reverse"
+				justifyContent="center"
+			>
+				<Button
+					m={3}
+					variant="color"
+					Icon={CheckAloneRegular}
+					outline
+					onClick={onAccept}
+				>
+					<Text>Accept</Text>
+				</Button>
+				<Button
+					m={3}
+					variant="error"
+					Icon={CloseRegular}
+					outline
+					onClick={onDecline}
+				>
+					<Text>Decline</Text>
+				</Button>
+			</Flex>
+		</PendingRequestContainer>
 	)
 }
