32b9fb5626e91eb7849898db4b5b8f906cd1e942
Hugo Morosini <hugo.morosini@gmail.com>
2022-08-17 13:35:54 +0200

    prevent zoom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 // import { appWithTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
 import { StyleProvider } from '@ledgerhq/react-ui'
@@ -7,10 +8,16 @@ import GlobalStyle from '@/styles/globalStyle'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const router = useRouter()
-	const { theme = "dark" } = router.query;
+	const { theme = 'dark' } = router.query
 
 	return (
 		<>
+			<Head>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, maximum-scale=1"
+				/>
+			</Head>
 			<StyleProvider
 				selectedPalette={theme as ThemeNames | undefined}
 				fontsPath="/fonts"
