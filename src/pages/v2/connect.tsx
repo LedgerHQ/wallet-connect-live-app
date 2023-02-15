84d8f863cb202fd45e717369e927a6b6c8208b49
Martin Cayuelas <martin.cayuelas@ledger.fr>
2023-02-15 17:15:00 +0100

    Remove Disconnect all button

--- src/pages/v2/connect.tsx
+++ src/pages/v2/connect.tsx
@@ -4,42 +4,25 @@ import {
 } from '@/components/WalletConnect/v2/components/GenericRow'
 import { formatUrl } from '@/components/WalletConnect/v2/utils/HelperUtil'
 import { web3wallet } from '@/components/WalletConnect/v2/utils/WalletConnectUtil'
-import { Box, Button, Flex, Text } from '@ledgerhq/react-ui'
+import { Box, Flex, Text } from '@ledgerhq/react-ui'
 import { SessionTypes } from '@walletconnect/types'
 import { useCallback, useEffect, useState } from 'react'
 import Image from 'next/image'
-import { useTranslation } from 'next-i18next'
 import useNavigation from '@/components/WalletConnect/v2/hooks/useNavigation'
 import {
-	ButtonsContainer,
 	List,
 	WalletConnectContainer,
 } from '@/components/WalletConnect/v2/components/Containers/util'
 export { getServerSideProps } from '../../lib/serverProps'
 
 export default function Connect() {
-	const { t } = useTranslation()
 	const { navigate, routes } = useNavigation()
 	const [sessions, setSessions] = useState<[string, SessionTypes.Struct][]>(
 		[],
 	)
 	useEffect(() => {
 		setSessions(Object.entries(web3wallet.getActiveSessions()))
-	}, [])
-
-	const handleDisconnectAll = useCallback(() => {
-		sessions.forEach(([, value]) => {
-			web3wallet.disconnectSession({
-				topic: value.topic,
-				reason: {
-					code: 3,
-					message: 'Disconnect Session',
-				},
-			})
-		})
-
-		setSessions([])
-	}, [])
+	}, [web3wallet.getActiveSessions()])
 
 	const goToDetailSession = useCallback((topic: string) => {
 		navigate(routes.sessionDetails, topic)
@@ -81,17 +64,6 @@ export default function Connect() {
 					</Box>
 				))}
 			</List>
-			<ButtonsContainer mt={4}>
-				<Button variant="shade" flex={1} onClick={handleDisconnectAll}>
-					<Text
-						variant="body"
-						fontWeight="semiBold"
-						color="neutral.c100"
-					>
-						{t('sessions.disconnectAll')}
-					</Text>
-				</Button>
-			</ButtonsContainer>
 		</WalletConnectContainer>
 	)
 }
