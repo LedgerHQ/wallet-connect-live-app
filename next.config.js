79413b9a2f08f2acebe71cb093ab8b8403da9c46
Martin Cayuelas <martin.cayuelas@ledger.fr>
2023-07-21 11:19:19 +0200

    Update config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
+
+const { withSentryConfig } = require('@sentry/nextjs')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true',
 })
@@ -27,40 +29,13 @@ const nextConfig = {
 
 module.exports = withBundleAnalyzer(nextConfig)
 
+module.exports = withSentryConfig(module.exports, {
+	// For all available options, see:
+	// https://github.com/getsentry/sentry-webpack-plugin#options
 
-// Injected content via Sentry wizard below
+	// Suppresses source map uploading logs during build
+	silent: true,
 
-const { withSentryConfig } = require("@sentry/nextjs");
-
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-
-    org: "ledger",
-    project: "wallet-connect-live-app",
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: "/monitoring",
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  }
-);
+	org: 'ledger',
+	project: 'wallet-connect-live-app',
+})
