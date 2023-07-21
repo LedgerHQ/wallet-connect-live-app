373e41e45203a0d8530d60b1527f7754e332885f
Martin Cayuelas <martin.cayuelas@ledger.fr>
2023-07-21 10:28:59 +0200

    Update config

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -8,9 +8,7 @@ import * as Sentry from '@sentry/nextjs'
 Sentry.init({
 	dsn: process.env.SENTRY_DNS,
 
-	// Adjust this value in production, or use tracesSampler for greater control
-	tracesSampleRate: 1,
-
-	// Setting this option to true will print useful information to the console while you're setting up Sentry.
-	debug: false,
+	tracesSampleRate: 0, // https://docs.sentry.io/platforms/javascript/performance
+	autoSessionTracking: false, // https://docs.sentry.io/platforms/javascript/configuration/options/#auto-session-tracking
+	sendClientReports: false, // https://docs.sentry.io/platforms/javascript/configuration/options/#send-client-reports
 })
