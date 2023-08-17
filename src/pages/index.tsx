d3996bf11d0d9e00d7b95e17547015349282585e
Martin Cayuelas <martin.cayuelas@ledger.fr>
2023-08-17 10:45:39 +0200

    🛠  [SUPPORT] : Add Env variable on vercel for shutdown WC app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,10 @@ export { getServerSideProps } from '../lib/serverProps'
 const Index: NextPage = () => {
 	const router = useRouter()
 
+	const isApplicationDisabled = Boolean(
+		process.env.NEXT_PUBLIC_APPLICATION_DISABLED === 'true',
+	)
+
 	const { uri: rawURI, mode: rawInitialMode } = router.query
 
 	const uri = rawURI && typeof rawURI === 'string' ? rawURI : undefined
@@ -37,20 +41,26 @@ const Index: NextPage = () => {
 					content="Ledger WalletConnect"
 				/>
 			</Head>
-			<ApplicationDisabled />
-			{/* {isMounted ? (
-				<WalletApiClientProvider networks={networkConfigs}>
-					{(accounts, userId, walletInfo) => (
-						<WalletConnect
-							initialMode={initialMode}
-							initialURI={uri}
-							accounts={accounts}
-							userId={userId}
-							walletInfo={walletInfo}
-						/>
-					)}
-				</WalletApiClientProvider>
-			) : null} */}
+
+			{isApplicationDisabled ? (
+				<ApplicationDisabled />
+			) : (
+				<>
+					{isMounted ? (
+						<WalletApiClientProvider>
+							{(accounts, userId, walletInfo) => (
+								<WalletConnect
+									initialMode={initialMode}
+									initialURI={uri}
+									accounts={accounts}
+									userId={userId}
+									walletInfo={walletInfo}
+								/>
+							)}
+						</WalletApiClientProvider>
+					) : null}
+				</>
+			)}
 		</Container>
 	)
 }
