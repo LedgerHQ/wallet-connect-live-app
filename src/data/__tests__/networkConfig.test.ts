ea6c911e7561ecc0ad78652b232a041a40f62c7b
Martin Cayuelas <martin.cayuelas@ledger.fr>
2023-10-11 10:40:44 +0200

    Add more coverage for  networks

--- src/data/__tests__/networkConfig.test.ts
+++ src/data/__tests__/networkConfig.test.ts
@@ -20,9 +20,47 @@ describe("SUPPORTED_NETWORK", () => {
     expect(polygon?.displayName).toBe("Polygon");
   });
 
+  it("contains Optimism", () => {
+    const optimism: Network | undefined = SUPPORTED_NETWORK["optimism"];
+    expect(optimism).toBeDefined();
+    expect(optimism?.displayName).toBe("Optimism");
+  });
+
+  it("contains Arbitrum", () => {
+    const arbitrum: Network | undefined = SUPPORTED_NETWORK["arbitrum"];
+    expect(arbitrum).toBeDefined();
+    expect(arbitrum?.displayName).toBe("Arbitrum");
+  });
+
+  it("contains Avalanche C-Chain", () => {
+    const avalancheCChain: Network | undefined = SUPPORTED_NETWORK["avalanche_c_chain"];
+    expect(avalancheCChain).toBeDefined();
+    expect(avalancheCChain?.displayName).toBe("Avalanche C-Chain");
+  });
+
+  it("contains Base", () => {
+    const base: Network | undefined = SUPPORTED_NETWORK["base"];
+    expect(base).toBeDefined();
+    expect(base?.displayName).toBe("Base");
+  });
+});
+
+describe("EIP155_GOERLI_CHAINS", () => {
   it("contains Ethereum Goerli", () => {
-    const goerli: Network | undefined = SUPPORTED_NETWORK["ethereum_goerli"];
-    expect(goerli).toBeDefined();
-    expect(goerli?.displayName).toBe("Ethereum Goerli");
+    const ethereumGoerli: Network | undefined = SUPPORTED_NETWORK["ethereum_goerli"];
+    expect(ethereumGoerli).toBeDefined();
+    expect(ethereumGoerli?.displayName).toBe("Ethereum Goerli");
+  });
+
+  it("contains Optimism Goerli", () => {
+    const optimismGoerli: Network | undefined = SUPPORTED_NETWORK["optimism_goerli"];
+    expect(optimismGoerli).toBeDefined();
+    expect(optimismGoerli?.displayName).toBe("Optimism Goerli");
+  });
+
+  it("contains Base Goerli", () => {
+    const baseGoerli: Network | undefined = SUPPORTED_NETWORK["base_goerli"];
+    expect(baseGoerli).toBeDefined();
+    expect(baseGoerli?.displayName).toBe("Base Goerli");
   });
 });
