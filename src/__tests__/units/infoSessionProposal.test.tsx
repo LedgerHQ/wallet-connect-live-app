a94f71ce9a35a1c244a52f26c4ab7a05fca065f3
Martin Cayuelas <martin.cayuelas@ledger.fr>
2023-10-11 09:41:34 +0200

    Add test for infoSessioNDetail

---
+++ src/__tests__/units/infoSessionProposal.test.tsx
@@ -0,0 +1,23 @@
+import { InfoSessionProposal } from "@/components/screens/sessions/sessionProposal/InfoSessionProposal";
+import { render, screen } from "@/tests-tools/test.utils";
+import React from "react";
+
+describe("InfoSessionProposal", () => {
+  it("renders in session details", () => {
+    render(<InfoSessionProposal isInSessionDetails={true} />);
+
+    expect(screen.getByText("sessionProposal.info2")).toBeInTheDocument();
+  });
+
+  it("renders in normal session", () => {
+    render(<InfoSessionProposal />);
+
+    expect(screen.getByText("sessionProposal.info")).toBeInTheDocument();
+  });
+
+  it("renders two bullet points", () => {
+    render(<InfoSessionProposal />);
+    const bulletPoints = screen.getAllByText(/sessionProposal.infoBullet./i);
+    expect(bulletPoints).toHaveLength(2);
+  });
+});
