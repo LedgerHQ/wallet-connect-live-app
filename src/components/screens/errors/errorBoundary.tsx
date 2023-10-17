944c1a2c9f1f41b4a4dda766a6431b745262471a
Martin Cayuelas <martin.cayuelas@ledger.fr>
2023-10-17 13:28:16 +0200

    WIP: Error Boundary

---
+++ src/components/screens/errors/errorBoundary.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { captureException } from "@sentry/nextjs";
+import { ErrorInfo } from "react";
+
+type Props = {
+  fallback: React.ReactNode;
+  children: React.ReactNode;
+};
+
+type State = {
+  hasError: boolean;
+};
+export class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, _info: ErrorInfo) {
+    // Example "componentStack":
+    //   in ComponentThatThrows (created by App)
+    //   in ErrorBoundary (created by App)
+    //   in div (created by App)
+    //   in App
+    console.log("componentDidCatch-----------");
+    //captureException(error, {});
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
