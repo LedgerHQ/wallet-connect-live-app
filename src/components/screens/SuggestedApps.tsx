1c8410787afc50d6f8e8e22048e2ad350c894a86
RamyEB <ramyelb@gmail.com>
2024-05-16 18:07:54 +0200

    feat: add sections

---
+++ src/components/screens/SuggestedApps.tsx
@@ -0,0 +1,84 @@
+import { device } from "@/styles/breakpoints";
+import AppCard from "../AppCard";
+import { Text } from "@ledgerhq/react-ui";
+import { t } from "i18next";
+import styled from "styled-components";
+
+const appList = [
+  {
+    id: "test",
+    name: "Opensea.io",
+    category: "NFT",
+    icon: "https://cdn.live.ledger.com/icons/platform/1inch.png",
+    url: "https://dapp-browser.apps.ledger.com/v2/",
+  },
+  {
+    id: "test",
+    name: "Opensea.io",
+    category: "NFT",
+    icon: "https://cdn.live.ledger.com/icons/platform/1inch.png",
+    url: "https://dapp-browser.apps.ledger.com/v2/",
+  },
+  {
+    id: "test",
+    name: "Opensea.io",
+    category: "NFT",
+    icon: "https://cdn.live.ledger.com/icons/platform/1inch.png",
+    url: "https://dapp-browser.apps.ledger.com/v2/",
+  },
+  {
+    id: "test",
+    name: "Opensea.io",
+    category: "NFT",
+    icon: "https://cdn.live.ledger.com/icons/platform/1inch.png",
+    url: "https://dapp-browser.apps.ledger.com/v2/",
+  },
+  {
+    id: "test",
+    name: "Opensea.io",
+    category: "NFT",
+    icon: "https://cdn.live.ledger.com/icons/platform/1inch.png",
+    url: "https://dapp-browser.apps.ledger.com/v2/",
+  },
+  {
+    id: "test",
+    name: "Opensea.io",
+    category: "NFT",
+    icon: "https://cdn.live.ledger.com/icons/platform/1inch.png",
+    url: "https://dapp-browser.apps.ledger.com/v2/",
+  },
+];
+
+const GridContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  gap: 10px;
+  justify-items: center;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+
+  @media ${device.mobile} {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media ${device.desktop} {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
+
+export default function SuggestedApps() {
+  return (
+    <>
+      <Text variant="extraSmall" color="neutral.c70" marginBottom={8}>
+        {t("sessions.apps.dontKnow")}
+      </Text>
+
+      <GridContainer>
+        {appList.map((app) => (
+          // eslint-disable-next-line react/jsx-key
+          <AppCard app={app} />
+        ))}
+      </GridContainer>
+    </>
+  );
+}
