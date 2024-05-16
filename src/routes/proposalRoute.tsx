a75858e182ddb62828ebf2b60e19917d40a72241
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-05-16 17:32:13 +0200

    add note about mock

--- src/routes/proposalRoute.tsx
+++ src/routes/proposalRoute.tsx
@@ -5,7 +5,7 @@ import { useMemo } from "react";
 import { useAtomValue } from "jotai";
 import { web3walletAtom } from "@/store/web3wallet.store";
 import usePendingProposals from "@/hooks/usePendingProposals";
-import sessionProposals from "@/data/mocks/sessionProposals";
+// import sessionProposals from "@/data/mocks/sessionProposals";
 
 export const proposalRoute = createRoute({
   getParentRoute: () => rootRoute,
@@ -15,6 +15,7 @@ export const proposalRoute = createRoute({
 
     const web3wallet = useAtomValue(web3walletAtom);
     const pendingProposals = usePendingProposals(web3wallet);
+    // NOTE: keep for pair review to quickly go through main connection flows.
     // const proposal = sessionProposals.noSupport;
     // const proposal = sessionProposals.required;
     // const proposal = sessionProposals.requiredMissingOne;
