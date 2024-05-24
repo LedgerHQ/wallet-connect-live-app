0180701d1d3d7dc928e3820580a0a4b3361b4d27
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-05-24 10:32:15 +0200

    i18n relayerstatus

--- src/components/screens/RelayerStatus.tsx
+++ src/components/screens/RelayerStatus.tsx
@@ -2,26 +2,35 @@ import { BoxedIcon, Flex, Icons } from "@ledgerhq/react-ui/index";
 import { StatusDot } from "../atoms/statusDot/StatusDot";
 import { useAtomValue } from "jotai";
 import { web3walletAtom } from "@/store/web3wallet.store";
+import { useTranslation } from "react-i18next";
 
-export function RelayerStatus() {
+export default function RelayerStatus() {
+  const { t } = useTranslation();
   const web3wallet = useAtomValue(web3walletAtom);
 
-if (!web3wallet?.core?.relayer || typeof web3wallet.core.relayer !== "object") {
+  if (
+    !web3wallet?.core?.relayer ||
+    typeof web3wallet.core.relayer !== "object"
+  ) {
     return;
   }
 
   return (
     <Flex mt={6} justifyContent={"center"} flexDirection={"row"} columnGap={2}>
       {web3wallet.core.relayer.connected ? (
-        <StatusDot status="success">Ready to connect</StatusDot>
+        <StatusDot status="success">
+          {t("connect.relayerStatus.connected")}
+        </StatusDot>
       ) : web3wallet.core.relayer.connecting ? (
         <StatusDot status="loading">
-          Connecting to wallet connect server
+          {t("connect.relayerStatus.connecting")}
         </StatusDot>
       ) : (
         <>
           <Flex>
-            <StatusDot status="error">No internet connection</StatusDot>
+            <StatusDot status="error">
+              {t("connect.relayerStatus.offline")}
+            </StatusDot>
           </Flex>
           <BoxedIcon
             Icon={Icons.NetworkWarning}
