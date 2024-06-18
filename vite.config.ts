fd4b37343935bf0f9bcb9d6224c2782326bd1f63
Ramy EL-BEHEDY <ramyelb@gmail.com>
2024-06-18 14:55:37 +0200

    fix: port

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import inject from "@rollup/plugin-inject";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3001,
+    port: 3000,
   },
   build: {
     sourcemap: true, // Source map generation must be turned on
