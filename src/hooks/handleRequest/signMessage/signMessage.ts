810f8e777aa234e29c04460b065c1afc017ace8d
Moustafa Koterba <moustafa.koterba@ledger.fr>
2025-05-07 18:14:45 +0200

    feat: Add support for Solana in WC Live App

--- src/hooks/handleRequest/signMessage/signMessage.ts
+++ src/hooks/handleRequest/signMessage/signMessage.ts
@@ -2,12 +2,12 @@ import { SOLANA_REQUESTS } from "@/data/methods/Solana.methods";
 import {
   acceptRequest,
   rejectRequest,
-  formatMessage,
   Errors,
 } from "@/hooks/handleRequest/utils";
 import { getAccountWithAddressAndChainId } from "@/utils/generic";
 import { Account, WalletAPIClient } from "@ledgerhq/wallet-api-client";
 import { IWalletKit } from "@reown/walletkit";
+import base58 from "bs58";
 
 export async function signMessage(
   request: SOLANA_REQUESTS,
@@ -29,13 +29,18 @@ export async function signMessage(
     request.params.pubkey,
     "solana",
   );
+
   if (accountSign) {
     try {
+      const message = base58.decode(request.params.message);
       const signedMessage = await client.message.sign(
         accountSign.id,
-        Buffer.from(request.params.message),
+        Buffer.from(message),
       );
-      await acceptRequest(walletKit, topic, id, formatMessage(signedMessage));
+
+      await acceptRequest(walletKit, topic, id, {
+        signature: base58.encode(signedMessage),
+      });
     } catch (_error) {
       await rejectRequest(walletKit, topic, id, Errors.txDeclined);
     }
